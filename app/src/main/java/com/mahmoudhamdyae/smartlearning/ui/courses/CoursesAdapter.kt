5209c9d9913b6b6a80de1cd3ff4132d19de2139f
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-02-01 16:43:45 +0200

    Courses Items in Recycler View

--- app/src/main/java/com/mahmoudhamdyae/smartlearning/ui/courses/CoursesAdapter.kt
+++ app/src/main/java/com/mahmoudhamdyae/smartlearning/ui/courses/CoursesAdapter.kt
@@ -1,6 +1,5 @@
 package com.mahmoudhamdyae.smartlearning.ui.courses
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
