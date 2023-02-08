273a4d07235095879558a36a1e0b215150b4904d
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-02-08 08:58:40 +0200

    Quizzes

--- app/src/main/java/com/mahmoudhamdyae/smartlearning/ui/course/CourseFragment.kt
+++ app/src/main/java/com/mahmoudhamdyae/smartlearning/ui/course/CourseFragment.kt
@@ -5,18 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.mahmoudhamdyae.smartlearning.R
-import com.mahmoudhamdyae.smartlearning.base.BaseFragment
 import com.mahmoudhamdyae.smartlearning.data.models.Course
 import com.mahmoudhamdyae.smartlearning.databinding.FragmentCourseBinding
 
-class CourseFragment: BaseFragment() {
+class CourseFragment: Fragment() {
 
     private lateinit var binding: FragmentCourseBinding
-    override val viewModel: CourseViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +22,6 @@ class CourseFragment: BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentCourseBinding.inflate(inflater)
-        binding.lifecycleOwner = this
-        binding.viewModel = viewModel
 
         binding.toolbar.inflateMenu(R.menu.menu_details)
 
@@ -38,7 +34,9 @@ class CourseFragment: BaseFragment() {
         val course = CourseFragmentArgs.fromBundle(requireArguments()).course
         val user = CourseFragmentArgs.fromBundle(requireArguments()).user
 
-        getUserType()
+        if (!user.teacher) {
+            binding.addStudentCard.visibility = View.GONE
+        }
 
         binding.toolbar.title = course!!.courseName
         binding.toolbar.setOnClickListener {
--- app/src/main/java/com/mahmoudhamdyae/smartlearning/ui/course/CourseViewModel.kt
+++
@@ -1,5 +0,0 @@
-package com.mahmoudhamdyae.smartlearning.ui.course
-
-import com.mahmoudhamdyae.smartlearning.base.BaseViewModel
-
-class CourseViewModel : BaseViewModel()
