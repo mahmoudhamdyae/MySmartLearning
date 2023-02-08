15424cd8d8bb458e7be9bc51e09ccea8c1ecd720
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-02-08 11:35:50 +0200

    Welcome Screen

--- app/src/main/java/com/mahmoudhamdyae/smartlearning/ui/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/mahmoudhamdyae/smartlearning/ui/welcome/WelcomeFragment.kt
@@ -7,6 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
+import com.mahmoudhamdyae.smartlearning.R
 import com.mahmoudhamdyae.smartlearning.databinding.FragmentWelcomeBinding
 import com.mahmoudhamdyae.smartlearning.utils.Constants
 
@@ -26,9 +29,52 @@ class WelcomeFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.toLogIn.setOnClickListener {
+//        binding.nextButton.setOnClickListener {
+////            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToLogInFragment())
+//            changeFirstTime()
+//        }
+
+        binding.viewPager.adapter = WelcomeViewPageAdapter()
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+        }.attach()
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                if (position == MAX_STEP - 1) {
+                    binding.nextButton.text = getString(R.string.welcome_get_started_button)
+                    binding.nextButton.contentDescription = getString(R.string.welcome_get_started_button)
+                } else {
+                    binding.nextButton.text = getString(R.string.welcome_next_button)
+                    binding.nextButton.contentDescription = getString(R.string.welcome_next_button)
+                }
+            }
+        })
+//        binding.viewPager.setPageTransformer(ZoomOutPageTransformer())
+
+        binding.skipButton.setOnClickListener {
             findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToLogInFragment())
             changeFirstTime()
+        }
+
+        binding.nextButton.setOnClickListener {
+            if (binding.nextButton.text.toString() == getString(R.string.welcome_get_started_button)) {
+                findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToLogInFragment())
+                changeFirstTime()
+            } else {
+                val current = (binding.viewPager.currentItem) + 1
+                binding.viewPager.currentItem = current
+
+                // Update next button
+                if (current == MAX_STEP - 1) {
+                    binding.nextButton.text = getString(R.string.welcome_get_started_button)
+                    binding.nextButton.contentDescription = getString(R.string.welcome_get_started_button)
+                } else {
+                    binding.nextButton.text = getString(R.string.welcome_next_button)
+                    binding.nextButton.contentDescription = getString(R.string.welcome_next_button)
+                }
+            }
         }
     }
 
@@ -38,5 +84,9 @@ class WelcomeFragment: Fragment() {
             putBoolean(Constants.FIRSTTIME, false)
             apply()
         }
+    }
+
+    companion object {
+        const val MAX_STEP = 3
     }
 }
---
+++ app/src/main/java/com/mahmoudhamdyae/smartlearning/ui/welcome/WelcomeViewPageAdapter.kt
@@ -0,0 +1,41 @@
+package com.mahmoudhamdyae.smartlearning.ui.welcome
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mahmoudhamdyae.smartlearning.R
+import com.mahmoudhamdyae.smartlearning.databinding.OnboardingItemBinding
+import com.mahmoudhamdyae.smartlearning.ui.welcome.WelcomeFragment.Companion.MAX_STEP
+
+class WelcomeViewPageAdapter : RecyclerView.Adapter<PagerVH2>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerVH2 {
+        return PagerVH2(
+            OnboardingItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = MAX_STEP
+
+    override fun onBindViewHolder(holder: PagerVH2, position: Int) = holder.itemView.run {
+        with(holder) {
+            if (position == 0) {
+                bindingDesign.introTitle.text = context.getString(R.string.select_account_type)
+                bindingDesign.introDescription.text = context.getString(R.string.select_account_type)
+                bindingDesign.introImage.setImageResource(R.drawable.school)
+            }
+            if (position == 1) {
+                bindingDesign.introTitle.text = context.getString(R.string.select_account_type)
+                bindingDesign.introDescription.text = context.getString(R.string.select_account_type)
+                bindingDesign.introImage.setImageResource(R.drawable.school)
+            }
+            if (position == 2) {
+                bindingDesign.introTitle.text = context.getString(R.string.select_account_type)
+                bindingDesign.introDescription.text = context.getString(R.string.select_account_type)
+                bindingDesign.introImage.setImageResource(R.drawable.school)
+            }
+        }
+    }
+}
+
+class PagerVH2 (val bindingDesign: OnboardingItemBinding) : RecyclerView.ViewHolder(bindingDesign.root)
